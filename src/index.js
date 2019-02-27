df59492eee208ad41ac335723bf256af9dd54636
rajan <rajangreedge@gmail.com>
2019-02-27 20:08:32 +0530

    another 2 commnet

--- src/index.js
+++ src/index.js
@@ -19,7 +19,36 @@ const App = ()=> {
                     <div className="text">Nice blog post !</div> 
                 </div>
             </div>
-
+            {/**2nd commnet */}
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="Avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM </span>
+                    </div>
+                    <div className="text">Nice blog post !</div> 
+                </div>
+            </div>
+            {/**3rd comment */}
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="Avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00PM </span>
+                    </div>
+                    <div className="text">Nice blog post !</div> 
+                </div>
+            </div>
         </div>
     );
 }
