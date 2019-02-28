7427da940ce63c204d841be7a380711f46dcce17
rajan <rajangreedge@gmail.com>
2019-02-28 18:32:56 +0530

    passed other props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import faker from 'faker';
-import CommentDetail from './CommentDetails';
+import faker from "faker";
+import CommentDetail from "./CommentDetails";
 
-const App = ()=> {
-    return (
-        <div className="ui container comments">
-            <CommentDetail author="Sam"/> 
-            <CommentDetail author="Rajan"/> 
-            <CommentDetail author="Durgesh"/> 
-        </div>
-    );
-}
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        comments="Nice blog!"
+        imgSrc={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Rajan"
+        timeAgo="Today at 2:00AM"
+        comments="Good Stuf"
+        imgSrc={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Durgesh"
+        timeAgo="Today at 12:01AM"
+        comments="Nice One"
+        imgSrc={faker.image.avatar()}
+      />
+    </div>
+  );
+};
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
