351d95a7dc85484cef6c32d8a52982cab29e357c
rajan <rajangreedge@gmail.com>
2019-02-28 18:11:31 +0530

    one comment with CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,54 +1,12 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import faker from 'faker';
+import CommentDetail from './CommentDetails';
 
 const App = ()=> {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="Avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM </span>
-                    </div>
-                    <div className="text">Nice blog post !</div> 
-                </div>
-            </div>
-            {/**2nd commnet */}
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="Avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM </span>
-                    </div>
-                    <div className="text">Nice blog post !</div> 
-                </div>
-            </div>
-            {/**3rd comment */}
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="Avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM </span>
-                    </div>
-                    <div className="text">Nice blog post !</div> 
-                </div>
-            </div>
+            <CommentDetail /> 
         </div>
     );
 }
